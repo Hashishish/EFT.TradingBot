faab0e4f433a299a5de1bf3a6f44743bf7e68251
Hashishish <df.dd.2@bk.ru>
2023-04-28 23:46:00 +0700

    module rewritten and extended with ChatGPT

--- tracker.py
+++ tracker.py
@@ -6,16 +6,24 @@ class MousePosition(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Mouse Position")
-        self.geometry("250x25")
+        self.geometry("250x40")
         self.label = tk.Label(self, text="")
         self.label.pack()
+        self.color_label = tk.Label(self, text="")
+        self.color_label.pack()
 
-        # Запускаем функцию для обновления координат
+        # Запускаем функцию для обновления координат и цвета
         self.update_position()
 
     def update_position(self):
         x, y = pyautogui.position()
         self.label.config(text=f"X: {x}, Y: {y}")
+
+        # Получаем цвет пикселя под курсором
+        color = pyautogui.screenshot().getpixel((x, y))
+        hex_color = f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"
+        self.color_label.config(text=f"Color: {hex_color}")
+
         self.after(100, self.update_position)
 
 
