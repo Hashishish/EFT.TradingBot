25546356bdedc9a65f680530f95360ab3ed382ae
Hashishish <df.dd.2@bk.ru>
2023-04-28 23:54:35 +0700

    added square with pixel color

--- tracker.py
+++ tracker.py
@@ -6,11 +6,13 @@ class MousePosition(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Mouse Position")
-        self.geometry("250x40")
+        self.geometry("250x60")
         self.label = tk.Label(self, text="")
         self.label.pack()
         self.color_label = tk.Label(self, text="")
         self.color_label.pack()
+        self.color_square = tk.Canvas(self, width=20, height=20)
+        self.color_square.pack()
 
         # Запускаем функцию для обновления координат и цвета
         self.update_position()
@@ -23,6 +25,9 @@ class MousePosition(tk.Tk):
         color = pyautogui.screenshot().getpixel((x, y))
         hex_color = f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"
         self.color_label.config(text=f"Color: {hex_color}")
+
+        # Заполняем квадратик цветом пикселя
+        self.color_square.create_rectangle(0, 0, 20, 20, fill=hex_color, outline="")
 
         self.after(100, self.update_position)
 
