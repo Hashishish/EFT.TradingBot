4c0aab88d8578a484a62c0491370dd301958e001
Hashishish <df.dd.2@bk.ru>
2023-04-29 02:54:10 +0700

    changed delay method

--- macros.py
+++ macros.py
@@ -1,3 +1,4 @@
+import time
 import pyautogui as pag
 import keyboard
 
@@ -6,65 +7,65 @@ pag.FAILSAFE = False  # НЕ ТРОГАТЬ!
 
 class Macros:
     def __init__(self):
-        # установка начальных координат
-        self.position_1 = 1870, 165
-        self.position_2 = 2350, 240
-        self.position_3 = 1585, 650
+        # Установка переменных
+        self.position_1 = 1870, 165  # Позиция с фильтром по цене
+        self.position_2 = 2350, 240  # Позиция с кнопкой купить
+        self.position_3 = 1585, 650  # Позиция с кнопкой "все"
+        self.lotPosition = 2477, 204  # Позиция с точкой для проверки лота
+        self.lotPosition_color = 177, 185, 189  # Цвет в формате RGB для проверяемой точки
 
-        self.delay = 0.4  # задержка в секундах между действиями
+        self.work_key, self.stop_key = 'q', 'w'  # Клавиши управления
 
-        self.work_key, self.stop_key = 'q', 'w'  # клавиши управления
+        self.delay = 0.2  # Базовая задержка в секундах
+        self.click_delay = 0.1  # Задержка перед кликом в секундах
+        self.hold_time = 0.03  # Время удержания в секундах
 
-        self.click_delay = 0.1  # задержка перед кликом в секундах
+        keyboard.add_hotkey(self.work_key, lambda: self.work())  # Объявление горячей клавиши
 
-        self.hold_time = 0.03  # время удержания в секундах
-
-        keyboard.add_hotkey(self.work_key, lambda: self.work())  # объявление горячей клавиши
-
-        keyboard.wait(self.stop_key)  # ожидание нажатия кнопки остановки
+        keyboard.wait(self.stop_key)  # Ожидание нажатия кнопки остановки
         pag.alert('Выход из макроса.')
         exit()
 
     def click(self):
-        pag.PAUSE = self.click_delay  # задержка в нажатии
-        pag.mouseDown(button='left')  # нажать на левую кнопку мыши
+        time.sleep(self.click_delay)  # Задержка в нажатии
+        pag.mouseDown(button='left')  # Нажать на левую кнопку мыши
         print("Левая кнопка мыши нажата")
-        pag.PAUSE = self.hold_time  # задержка в отжатии
-        pag.mouseUp(button='left')  # отпустить левую кнопку мыши
+        time.sleep(self.hold_time)  # Задержка в отжатии
+        pag.mouseUp(button='left')  # Отпустить левую кнопку мыши
         print("Левая кнопка мыши отпущена")
 
     def update(self):
-        pag.moveTo(self.position_1[0], self.position_1[1])  # переместить мышь на позицию 1
+        pag.moveTo(self.position_1[0], self.position_1[1])  # Переместить мышь на позицию 1
         print("Курсор перемещён в 1 позицию")
+        time.sleep(self.click_delay * 5)  # задержка в обновлении
         self.click()
 
     def buy(self, full=False):
-        pag.moveTo(self.position_2[0], self.position_2[1], duration=self.delay)  # переместить мышь на позицию 2
+        pag.moveTo(self.position_2[0], self.position_2[1], duration=self.delay)  # Переместить мышь на позицию 2
         self.click()
 
         if full:
-            pag.moveTo(self.position_3[0], self.position_3[1], duration=self.delay / 4)  # переместить мышь на позицию 3
+            pag.moveTo(self.position_3[0], self.position_3[1], duration=self.delay)  # Переместить мышь на позицию 3
             self.click()
 
-        pag.PAUSE = self.delay  # задержка перед нажатием клавиши "y"
-        pag.press('y')  # нажать на клавишу "y"
+        time.sleep(self.delay / 2)  # Задержка перед нажатием клавиши "y"
+        pag.press('y')  # Нажать на клавишу "y"
         print("Нажата кнопка Y")
 
     def work(self):
         while True:
             print("НАЧАЛО ЦИКЛА")
 
-            if pag.screenshot().getpixel((2477, 204)) == (177, 185, 189):  # Проверка на наличие лота в списке
+            if pag.screenshot().getpixel(self.lotPosition) == self.lotPosition_color:  # Проверка на наличие лота
                 print("Появился новый лот")
                 self.buy()  # Покупается первый лот из списка
 
             self.update()  # Обновим список лотов
-            pag.PAUSE = self.click_delay  # задержка
 
             if keyboard.is_pressed(self.work_key):
                 print("Зажата рабочая кнопка")
-                pag.PAUSE = 2
-                print("Зажата рабочая кнопка уже в течение 2 секунд")
+                time.sleep(1)  # Ожидание нажатия продолжительностью 1 секунду
+                print("Зажата рабочая кнопка уже в течение 1 секунды")
 
                 if keyboard.is_pressed(self.work_key):
                     pag.alert('Цикл остановлен.')
