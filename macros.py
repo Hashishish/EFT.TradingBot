951abd1d6968ba196f33747bca59c9845a0e3600
Hashishish <df.dd.2@bk.ru>
2023-04-29 01:35:54 +0700

    refactoring and splitting actions into methods

--- macros.py
+++ macros.py
@@ -19,40 +19,49 @@ class Macros:
 
         self.hold_time = 0.03  # время удержания в секундах
 
-        keyboard.add_hotkey(self.work_key, lambda: self.buy())  # объявление горячей клавиши
+        keyboard.add_hotkey(self.work_key, lambda: self.work())  # объявление горячей клавиши
 
         keyboard.wait(self.stop_key)  # ожидание нажатия кнопки остановки
         pag.alert('Выход из макроса.')
         exit()
 
+    def update(self):
+        pag.moveTo(self.position_1[0], self.position_1[1])  # переместить мышь на позицию 1
+        print("Курсор перемещён в 1 позицию")
+        pag.mouseDown(button='left')  # нажать на левую кнопку мыши
+        print("Левая кнопка мыши нажата")
+        pag.PAUSE = self.hold_time  # задержка в отжатии
+        pag.mouseUp(button='left')  # отпустить левую кнопку мыши
+        print("Левая кнопка мыши отпущена")
+
     def buy(self):
+        pag.moveTo(self.position_2[0], self.position_2[1], duration=self.delay)  # переместить мышь на позицию 2
+        print("Курсор перемещён во 2 позицию")
+        pag.PAUSE = self.click_delay  # задержка в нажатии
+        pag.mouseDown(button='left')  # нажать на левую кнопку мыши
+        print("Левая кнопка мыши нажата")
+        pag.PAUSE = self.hold_time  # задержка в отжатии
+        pag.mouseUp(button='left')  # отпустить левую кнопку мыши
+        print("Левая кнопка мыши отпущена")
+        pag.PAUSE = self.delay  # задержка перед нажатием клавиши "y"
+        pag.press('y')  # нажать на клавишу "y"
+        print("Нажата кнопка Y")
+
+    def work(self):
         while True:
             print("НАЧАЛО ЦИКЛА")
-            if pag.screenshot().getpixel((2477, 204)) == (177, 185, 189):
+
+            if pag.screenshot().getpixel((2477, 204)) == (177, 185, 189):  # Проверка на наличие лота в списке
                 print("Появился новый лот")
-                pag.moveTo(self.position_2[0], self.position_2[1], duration=self.delay)  # переместить мышь на позицию 2
-                print("Курсор перемещён во 2 позицию")
-                pag.PAUSE = self.click_delay  # задержка в нажатии
-                pag.mouseDown(button='left')  # нажать на левую кнопку мыши
-                print("Левая кнопка мыши нажата")
-                pag.PAUSE = self.hold_time  # задержка в отжатии
-                pag.mouseUp(button='left')  # отпустить левую кнопку мыши
-                print("Левая кнопка мыши отпущена")
-                pag.PAUSE = self.delay  # задержка перед нажатием клавиши "y"
-                pag.press('y')  # нажать на клавишу "y"
-                print("Нажата кнопка Y")
-            pag.moveTo(self.position_1[0], self.position_1[1])  # переместить мышь на позицию 1
-            print("Курсор перемещён в 1 позицию")
-            pag.mouseDown(button='left')  # нажать на левую кнопку мыши
-            print("Левая кнопка мыши нажата")
-            pag.PAUSE = self.hold_time  # задержка в отжатии
-            pag.mouseUp(button='left')  # отпустить левую кнопку мыши
-            print("Левая кнопка мыши отпущена")
-            pag.PAUSE = self.click_delay  # задержка в нажатии
+                self.buy()  # Покупается первый лот из списка
+
+            pag.PAUSE = self.click_delay  # задержка
+
             if keyboard.is_pressed(self.work_key):
                 print("Зажата рабочая кнопка")
                 pag.PAUSE = 2
                 print("Зажата рабочая кнопка уже в течение 2 секунд")
+
                 if keyboard.is_pressed(self.work_key):
                     pag.alert('Цикл остановлен.')
                     print("Команда остановки цикла")
